b42634bc9d897f2b80d0a714637883448a223d95
Son Pham <son.pham@ocado.com>
2014-06-28 01:28:59 +0100

    Testimonials in Angular

--- js/controllers.js
+++ js/controllers.js
@@ -15,4 +15,17 @@ app.controller("StaffCtrl", function($scope) {
     $scope.guests = guests;
 }).controller("ScheduleCtrl", function($scope) {
     $scope.schedule = schedule;
+}).controller("TestimonialsCtrl", function($scope) {
+    var partitionSize = 3;
+    var testimonialGroup = [];
+    for (var i = 0; i < partitionSize; i += 1) {
+        testimonialGroup[i] = [];
+    }
+    for (key in testimonials) {
+        var testimonial = testimonials[key];
+        var partition = key % partitionSize;
+        testimonialGroup[partition].push(testimonial);
+    }
+    $scope.testimonialGroup = testimonialGroup;
+    $scope.partitionSize = partitionSize;
 });
--- js/data.js
+++ js/data.js
@@ -154,3 +154,30 @@ var schedule = [
         time: "11:00"
     }
 ];
+
+var testimonials = [
+    {
+        author: "Ut labore et dolore magna aliqua",
+        content: "MIND. BLOWN.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        author: "Ut labore et dolore magna aliqua",
+        content: "MIND. BLOWN.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        author: "Ut labore et dolore magna aliqua",
+        content: "MIND. BLOWN.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        author: "Ut labore et dolore magna aliqua",
+        content: "MIND. BLOWN.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        author: "Ut labore et dolore magna aliqua",
+        content: "MIND. BLOWN.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        author: "Ut labore et dolore magna aliqua",
+        content: "MIND. BLOWN.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    }
+]
