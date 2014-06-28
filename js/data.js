bb863fd521281459828d19a11f4d0c1d3e3decc2
Son Pham <son.pham@ocado.com>
2014-06-28 11:54:39 +0100

    Add more translation goodness to Guest

--- js/data.js
+++ js/data.js
@@ -76,31 +76,31 @@ var staff = [
 var guests = [
     {
         name: "Dr. Quack",
-        firstBioLine: "KXX CNNer, Some Uni Alumni",
-        secondBioLine: "CEO of Meh",
+        firstBioLine: "MOCK_BIO_1",
+        secondBioLine: "MOCK_BIO_2",
         country: "us",
-        back: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        back: "MOCK_BACK_INFO",
     },
     {
         name: "Dr. Quack",
-        firstBioLine: "KXX CNNer, Some Uni Alumni",
-        secondBioLine: "CEO of Meh",
+        firstBioLine: "MOCK_BIO_1",
+        secondBioLine: "MOCK_BIO_2",
         country: "us",
-        back: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        back: "MOCK_BACK_INFO",
     },
     {
         name: "Dr. Quack",
-        firstBioLine: "KXX CNNer, Some Uni Alumni",
-        secondBioLine: "CEO of Meh",
+        firstBioLine: "MOCK_BIO_1",
+        secondBioLine: "MOCK_BIO_2",
         country: "us",
-        back: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        back: "MOCK_BACK_INFO",
     },
     {
         name: "Dr. Quack",
-        firstBioLine: "KXX CNNer, Some Uni Alumni",
-        secondBioLine: "CEO of Meh",
+        firstBioLine: "MOCK_BIO_1",
+        secondBioLine: "MOCK_BIO_2",
         country: "us",
-        back: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+        back: "MOCK_BACK_INFO",
     }
 ]
 
