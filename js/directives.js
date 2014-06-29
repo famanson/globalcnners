49b07698921ebce145c2d508e189b1d9203d39db
Son Pham <son.pham@ocado.com>
2014-06-29 13:07:14 +0100

    Move a lot of things into directives

---
+++ js/directives.js
@@ -0,0 +1,65 @@
+
+app.directive('flippable', function() {
+    return {
+        // Restrict it to be an attribute in this case
+        restrict: 'A',
+        // responsible for registering DOM listeners as well as updating the DOM
+        link: function(scope, element, attrs) {
+            // Activate flippable info
+            $(element).bind("click", function() {
+                if($(this).hasClass("flipped")) {
+                    $(this).removeClass("flipped")
+                } else {
+                    $(this).addClass("flipped")
+                }
+            });
+        }
+    };
+}).directive('onStaffFinish', function ($timeout) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            if (scope.$last === true) {
+                $timeout(function () {
+                    scope.$emit("correctFlippableHeight");
+                    scope.$emit("renderWall");
+                });
+            }
+        }
+    }
+}).directive('flagPickerActivate', function() {
+    return {
+        // Restrict it to be an attribute in this case
+        restrict: 'A',
+        // responsible for registering DOM listeners as well as updating the DOM
+        link: function(scope, element, attrs) {
+            if (scope.flagFilterEnabled) {
+                $(element).bind("click", function() {
+                    var staffWall = scope.walls.staffWall; // See StaffCtrl in controllers.js
+                    var country = $(this).attr("country");
+                    if (!$(this).hasClass("grayscale") && $("#flag-picker").hasClass("filtered")) {
+                        $(this).siblings(".flag-icon").andSelf().removeClass("grayscale");
+                        $("#flag-tip-2").fadeOut(250, function() {
+                            $("#flag-tip-1").fadeIn(250);
+                        });
+                        staffWall.unFilter();
+                        $("#flag-picker").removeClass("filtered");
+                    } else {
+                        $(this).siblings(".flag-icon").andSelf().each(function() {
+                            if ($(this).attr("country") !== country) {
+                                $(this).addClass("grayscale");
+                            } else {
+                                $(this).removeClass("grayscale");
+                            }
+                        });
+                        staffWall.filter(".member." + country);
+                        $("#flag-tip-1").fadeOut(250, function() {
+                            $("#flag-tip-2").fadeIn(250);
+                        });
+                        $("#flag-picker").addClass("filtered");
+                    }
+                });
+            }
+        }
+    };
+});
